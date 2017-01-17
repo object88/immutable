f353f9faca2fbf9614c48ac2625663e2d07e7ceb
object88 <object88@gmail.com>
2017-01-17 07:49:26 -0700

    Adding go-routine supported iterative functions

--- routine.go
+++ routine.go
@@ -1,74 +1,87 @@
 package immutable
 
-type routine struct {
-	options *RoutineOptions
-	base    Base
-	funcs  []
-	err *error
-}
+import "fmt"
 
 type RoutineOptions struct {
 	Width int
 }
 
-type HashRoutine struct {
-	routine
+func (s *MappedStep) GoMap(f MapPredicate) *MappedStep {
+	hr.funcs = append(hr.funcs, &mapStep{predicate})
+	return hr
 }
 
-type ListRoutine struct {
-	routine
-}
-
-func (*HashRoutine) GoFilter(f FilterPredicate) *HashRoutine {
+func (s *MappedStep) GoReduce(f ReducePredicate) *ReducedStep {
 	return nil
 }
 
-func (hr *HashRoutine) GoMap(predicate MapPredicate) *HashRoutine {
-	// mutated := b.instantiate(b.Size(), nil)
-	// abort := make(chan struct{})
-	// ch := hr.base.iterate(abort)
-	// for kvp := range ch {
-	// 	newV, err := go predicate(kvp.key, kvp.value)
-	// 	if err != nil {
-	// 		hr.err = &err
-	// 		close(abort)
-	//
-	// 		return hr
-	// 	}
-	// 	mutated.internalSet(kvp.key, newV)
-	// }
-	// return hr
+func (s *MappedStep) ToHashmap() *HashMap {
+	return nil
 }
 
-func (*HashRoutine) GoReduce(acc Value, f ReducePredicate) (Value, error) {
-	return nil, nil
+// func (s *FilteredStep) ToList() interface{} {}
+// Func (*FilteredStep) ToSet() *Set {}
+
+type MappedStep struct {
 }
 
-func (*HashRoutine) Result() (*HashMap, error) {
-	return nil, nil
+type ReducedStep struct {
+}
+
+type routine struct {
+	options *RoutineOptions
+	base    Base
+	funcs   []step
+	err     *error
+}
+
+type step interface {
+	execute(kvp <-chan keyValuePair)
+}
+
+type mapStep struct {
+	f MapPredicate
+}
+
+func (ms *mapStep) execute(kvp <-chan keyValuePair) {
+	x := <-kvp
+	key, value := x.key, x.value
+	result, err := ms.f(key, value)
+}
+
+func (hr *HashRoutine) Result() (*HashMap, error) {
+	routineCount := 10
+	if routineCount > hr.base.Size() {
+		routineCount = hr.base.Size()
+	}
+
+	var err error
+	chans := make([]chan keyValuePair, routineCount)
+	abort := make(chan error)
+	ch := make(chan *HashMap)
+	go func() {
+		defer close(ch)
+
+		for _, v := range chans {
+		}
+
+		for i := 0; i < hr.base.Size(); i++ {
+			select {
+			case res := <-resc:
+				fmt.Println(res)
+			case err = <-abort:
+				return
+			}
+		}
+
+	}()
+
+	result := <-ch
+
+	return result, nil
 }
 
 func (*HashRoutine) ToList() (*ListRoutine, error) {
-	return nil, nil
-}
-
-func (*ListRoutine) GoFilter(f FilterPredicate) *ListRoutine {
-	return nil
-}
-
-func (*ListRoutine) GoMap(f MapPredicate) *ListRoutine {
-	return nil
-}
-
-func (*ListRoutine) GoReduce(acc Value, f ReducePredicate) (Value, error) {
-	return nil, nil
-}
-
-func (*ListRoutine) Result() (interface{}, error) {
-	return nil, nil
-}
-
-func (*ListRoutine) ToHashmap() (*HashMap, error) {
 	return nil, nil
 }
 
