bdfd3107c29397e2d6efdd32cb2e721829c71be7
object88 <object88@gmail.com>
2017-01-17 14:46:15 -0700

    Memory allocation test cleanup

--- memory/memory_test.go
+++ memory/memory_test.go
@@ -10,11 +10,18 @@ func Test_Small_AllocateMemories(t *testing.T) {
 		bits     uint32
 		count    uint32
 		expected int
-	}{}
+	}{
+		{1, 8, 1},
+		{8, 1, 1},
+		{5, 2, 2},
+		{7, 7, 7},
+		{7, 8, 7},
+		{7, 9, 8},
+	}
 	for index, tc := range testCases {
 		t.Run(fmt.Sprintf("%d", index), func(t *testing.T) {
 			m := AllocateMemories(SmallBlock, tc.bits, tc.count)
-			mem := m.(*Memories32).m
+			mem := m.(*Memories8).m
 			evaluate(t, len(mem), tc.expected)
 		})
 	}
@@ -38,50 +45,53 @@ func Test_Large_AllocateMemories(t *testing.T) {
 			evaluate(t, len(mem), tc.expected)
 		})
 	}
-	// evaluateLargeAllocate(t, 32, 1, 1)
-	// evaluateLargeAllocate(t, 32, 2, 2)
-	// evaluateLargeAllocate(t, 25, 1, 1)
-	// evaluateLargeAllocate(t, 25, 2, 2)
 }
 
 func Test_ExtraLarge_AllocateMemories(t *testing.T) {
-	evaluateExtraLargeAllocate(t, 3, 3, 1)
-	evaluateExtraLargeAllocate(t, 3, 22, 2)
-	evaluateExtraLargeAllocate(t, 31, 1, 1)
-	evaluateExtraLargeAllocate(t, 31, 2, 1)
-	evaluateExtraLargeAllocate(t, 31, 3, 2)
-	evaluateExtraLargeAllocate(t, 32, 1, 1)
-	evaluateExtraLargeAllocate(t, 32, 2, 1)
-	evaluateExtraLargeAllocate(t, 32, 3, 2)
-	evaluateExtraLargeAllocate(t, 33, 1, 1)
-	evaluateExtraLargeAllocate(t, 33, 2, 2)
-	evaluateExtraLargeAllocate(t, 33, 3, 2)
-	evaluateExtraLargeAllocate(t, 63, 1, 1)
-	evaluateExtraLargeAllocate(t, 63, 2, 2)
+	testCases := []struct {
+		bits     uint32
+		count    uint32
+		expected int
+	}{
+		{3, 3, 1},
+		{3, 22, 2},
+		{31, 1, 1},
+		{31, 2, 1},
+		{31, 3, 2},
+		{32, 1, 1},
+		{32, 2, 1},
+		{32, 3, 2},
+		{33, 1, 1},
+		{33, 2, 2},
+		{33, 3, 2},
+		{63, 1, 1},
+		{63, 2, 2},
+	}
+	for index, tc := range testCases {
+		t.Run(fmt.Sprintf("%d", index), func(t *testing.T) {
+			m := AllocateMemories(ExtraLargeBlock, tc.bits, tc.count)
+			mem := m.(*Memories64).m
+			evaluate(t, len(mem), tc.expected)
+		})
+	}
 }
 
 func Test_NoPacking_AllocateMemories(t *testing.T) {
-	evaluateNoPackingAllocate(t, 1, 1)
-	evaluateNoPackingAllocate(t, 2, 2)
-	evaluateNoPackingAllocate(t, 4, 4)
-}
-
-// func evaluateLargeAllocate(t *testing.T, bits, count uint32, expected int) {
-// 	m := AllocateMemories(LargeBlock, bits, count)
-// 	mem := m.(*Memories32).m
-// 	evaluate(t, len(mem), expected)
-// }
-
-func evaluateExtraLargeAllocate(t *testing.T, bits, count uint32, expected int) {
-	m := AllocateMemories(ExtraLargeBlock, bits, count)
-	mem := m.(*Memories64).m
-	evaluate(t, len(mem), expected)
-}
-
-func evaluateNoPackingAllocate(t *testing.T, count uint32, expected int) {
-	m := AllocateMemories(NoPacking, 0, count)
-	mem := m.(*MemoriesNoPacking).m
-	evaluate(t, len(mem), expected)
+	testCases := []struct {
+		count    uint32
+		expected int
+	}{
+		{1, 1},
+		{2, 2},
+		{4, 4},
+	}
+	for index, tc := range testCases {
+		t.Run(fmt.Sprintf("%d", index), func(t *testing.T) {
+			m := AllocateMemories(NoPacking, 0, tc.count)
+			mem := m.(*MemoriesNoPacking).m
+			evaluate(t, len(mem), tc.expected)
+		})
+	}
 }
 
 func evaluate(t *testing.T, result, expected int) {
