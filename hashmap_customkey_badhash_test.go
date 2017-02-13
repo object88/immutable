bf150c37f68f7831a30ed63720a16e7a93531c53
object88 <object88@gmail.com>
2017-02-12 17:00:14 -0700

    Working bad-hash test

--- hashmap_customkey_badhash_test.go
+++ hashmap_customkey_badhash_test.go
@@ -1,109 +1,111 @@
 package immutable
 
-// import (
-// 	"fmt"
-// 	"testing"
-//
-// 	"github.com/object88/immutable/core"
-// 	"github.com/object88/immutable/handlers/integers"
-// )
-//
-// // This suite of tests is designed to test the bucket overflow behavior.
-// // By using a `Hash` function which only ever resolves into one of two
-// // hash keys, all entries will end up in one or two buckets.  With a
-// // sufficiently large dataset, this will cause bucket overflow to be used.
-//
-// type MyBadElement int
-//
-// func (k MyBadElement) Hash(seed uint32) uint64 {
-// 	if k%2 == 0 {
-// 		return 0x0
-// 	}
-// 	return 0xffffffffffffffff
-// }
-//
-// func (k MyBadElement) String() string {
-// 	return fmt.Sprintf("%d", k)
-// }
-//
-// func WithMyBadElementMetadata(o *core.HashMapOptions) {
-// 	o.KeyConfig = &core.HandlerConfig{
-// 		Compare: func(a, b core.Element) (match bool) {
-// 			return int(a.(MyBadElement)) == int(b.(MyBadElement))
-// 		},
-// 		CreateBucket: func(count int) core.SubBucket {
-// 			m := make([]int, count)
-// 			return &MyBadElementSubBucket{m}
-// 		},
-// 	}
-// 	integers.WithIntValueMetadata(o)
-// }
-//
-// type MyBadElementSubBucket struct {
-// 	m []int
-// }
-//
-// func NewMyBadElementHashHandler() core.BucketGenerator {
-// 	return NewMyBadElementSubBucket
-// }
-//
-// func NewMyBadElementSubBucket(count int) core.SubBucket {
-// 	m := make([]int, count)
-// 	return &MyBadElementSubBucket{m}
-// }
-//
-// func (b *MyBadElementSubBucket) Hydrate(index int) core.Element {
-// 	u := b.m[index]
-// 	v := MyBadElement(u)
-// 	return v
-// }
-//
-// func (b *MyBadElementSubBucket) Dehydrate(index int, v core.Element) {
-// 	u := v.(MyBadElement)
-// 	b.m[index] = int(u)
-// }
-//
-// func Test_Hashmap_CustomKey_BadHash_Iterate(t *testing.T) {
-// 	max := 100
-// 	data := make(map[core.Element]core.Element, max)
-// 	for i := 0; i < max; i++ {
-// 		data[MyBadElement(i)] = integers.IntElement(0)
-// 	}
-// 	original := NewHashMap(data, WithMyBadElementMetadata)
-// 	if original == nil {
-// 		t.Fatal("NewHashMap returned nil\n")
-// 	}
-// 	size := original.Size()
-// 	if size != len(data) {
-// 		t.Fatalf("Incorrect size; expected %d, got %d\n", len(data), size)
-// 	}
-// 	original.ForEach(func(k core.Element, v core.Element) {
-// 		if v.(integers.IntElement) == 1 {
-// 			t.Fatalf("At %s, already visited\n", k)
-// 		}
-// 		data[k] = integers.IntElement(1)
-// 	})
-//
-// 	for k, v := range data {
-// 		if v.(integers.IntElement) != 1 {
-// 			t.Fatalf("At %s, not visited\n", k)
-// 		}
-// 	}
-// }
-//
-// func Test_Hashmap_CustomKey_BadHash_Get(t *testing.T) {
-// 	max := 100
-// 	contents := make(map[core.Element]core.Element, max)
-// 	for i := 0; i < max; i++ {
-// 		contents[MyBadElement(i)] = integers.IntElement(i)
-// 	}
-//
-// 	original := NewHashMap(contents, WithMyBadElementMetadata)
-//
-// 	for k, v := range contents {
-// 		result, _, _ := original.Get(k)
-// 		if result != v {
-// 			t.Fatalf("At %s, expected %s, got %s\n%#v", k, v, result, original)
-// 		}
-// 	}
-// }
+import (
+	"fmt"
+	"testing"
+	"unsafe"
+
+	"github.com/object88/immutable/core"
+	"github.com/object88/immutable/handlers/strings"
+)
+
+// This suite of tests is designed to test the bucket overflow behavior.
+// By using a `Hash` function which only ever resolves into one of two
+// hash keys, all entries will end up in one or two buckets.  With a
+// sufficiently large dataset, this will cause bucket overflow to be used.
+
+type MyBadHandler struct{}
+
+func (MyBadHandler) Compare(a, b unsafe.Pointer) (match bool) {
+	return *(*int)(a) == *(*int)(b)
+}
+
+func (h MyBadHandler) CompareTo(memory unsafe.Pointer, index int, other unsafe.Pointer) (match bool) {
+	p := h.Read(memory, index)
+	return h.Compare(p, other)
+}
+
+func (MyBadHandler) CreateBucket(count int) unsafe.Pointer {
+	m := make([]int, count)
+	return unsafe.Pointer(&m)
+}
+
+func (MyBadHandler) Hash(ep unsafe.Pointer, seed uint32) uint64 {
+	e := *(*int)(ep)
+	if e%2 == 0 {
+		return 0x0
+	}
+	return 0xffffffffffffffff
+}
+
+func (MyBadHandler) Read(memory unsafe.Pointer, index int) (result unsafe.Pointer) {
+	m := *(*[]int)(memory)
+	return unsafe.Pointer(&(m[index]))
+}
+
+func (MyBadHandler) Format(value unsafe.Pointer) string {
+	return fmt.Sprintf("%d", *(*int)(value))
+}
+
+func (MyBadHandler) Write(memory unsafe.Pointer, index int, value unsafe.Pointer) {
+	m := *(*[]int)(memory)
+	m[index] = *(*int)(value)
+}
+
+func WithMyBadElementMetadata(o *core.HashMapOptions) {
+	var handler MyBadHandler
+	o.KeyConfig = handler
+	strings.WithStringValueMetadata(o)
+}
+
+func Test_Hashmap_CustomKey_BadHash_Iterate(t *testing.T) {
+	max := 100
+	data := make(map[int]string, max)
+	for i := 0; i < max; i++ {
+		data[i] = "0"
+	}
+	contents := make(map[unsafe.Pointer]unsafe.Pointer, max)
+	for k, v := range data {
+		key, value := k, v
+		contents[unsafe.Pointer(&key)] = unsafe.Pointer(&value)
+	}
+	original := NewHashMap(contents, WithMyBadElementMetadata)
+	if original == nil {
+		t.Fatal("NewHashMap returned nil\n")
+	}
+	size := original.Size()
+	if size != len(data) {
+		t.Fatalf("Incorrect size; expected %d, got %d\n", len(data), size)
+	}
+	original.ForEach(func(kp, _ unsafe.Pointer) {
+		k := *(*int)(kp)
+		if data[k] == "1" {
+			t.Fatalf("At %d, already visited\n", k)
+		}
+		data[k] = "1"
+	})
+
+	for k, v := range data {
+		if v != "1" {
+			t.Fatalf("At %d, not visited\n", k)
+		}
+	}
+}
+
+func Test_Hashmap_CustomKey_BadHash_Get(t *testing.T) {
+	max := 100
+	contents := make(map[unsafe.Pointer]unsafe.Pointer, max)
+	for i := 0; i < max; i++ {
+		s := fmt.Sprintf("%d", i)
+		contents[unsafe.Pointer(&i)] = unsafe.Pointer(&s)
+	}
+
+	original := NewHashMap(contents, WithMyBadElementMetadata)
+
+	for k, v := range contents {
+		result, _, _ := original.Get(k)
+		if result != v {
+			t.Fatalf("At %d, expected %s, got %s\n%#v", k, *(*string)(v), *(*string)(result), original)
+		}
+	}
+}
